ede276ea38324df7e0d96fea4c7896e79f935bb0
alexayca <90019860+alexayca@users.noreply.github.com>
2022-01-27 01:12:58 -0600

    another practice

---
+++ examples/conversor_medidas.py
@@ -0,0 +1,26 @@
+# Convertir longitudes inglesas a sistema metrico decimal
+
+
+def to_inch(length):
+    inch_to_meter=0.0254
+    
+
+
+def to_foot(length):
+    foot_to_meter=0.3048
+    
+
+def to_yard(length):
+    yard_to_meter=0.9144
+    
+
+
+def to_mile(length):
+    mile_to_meter=1609.344
+    
+
+
+def to_nautical_mile(length):
+    nautical_to_meter=1852
+    #pass
+
